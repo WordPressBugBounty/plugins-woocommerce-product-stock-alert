eda0abc17ee49bab52ad7ddc49c525709eac25bc
Sean Murphy <sean@iamseanmurphy.com>
2025-07-23 07:19:37 +0000

    3.0.4

--- woocommerce-product-stock-alert/assets/js/index.asset.php
+++ woocommerce-product-stock-alert/assets/js/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f9dfccada975adf27d6d');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f5ac5a1deb3800bfc3ac');
